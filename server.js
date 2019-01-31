30fa4d6b4183b53131b610c3dcd86b10520d8439
Starli0n <Starli0n@users.noreply.github.com>
2019-01-31 18:05:47 +0100

    Move the logic to the server

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // Public
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Api
 app.get('/api', (req, res) => {
-  res.send('Hello World from the api\n');
+  res.json({message: 'Hello World from the api'});
+});
+
+app.get('/api/sum/:numberA/:numberB', function (req, res) {
+  var numberA = parseInt(req.params.numberA);
+  var numberB = parseInt(req.params.numberB);
+  var sum = numberA + numberB;
+  res.send({result: sum});
 });
 
 app.listen(PORT, HOST);
